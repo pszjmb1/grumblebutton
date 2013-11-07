a5e49a76adcacf985601386403a28a2463b3820e
JMB <jmb@jmb-VirtualBox.(none)>
2013-11-07 11:05:49 +0000

    Using collection for issues and populating with fixture data.

--- client/views/grumbles/issues.js
+++ client/views/grumbles/issues.js
@@ -1,49 +1,5 @@
-var issuesFixture = [
-	{
-		date: '01/01/2014',
-		time: '11:52',
-		dept: 'Bone Densitometry',
-		unit: 'Physiotherapy',
-		room: 'P4',
-		urgency: 'medium',
-		category: 'utility',
-		subcategory: 'heating',
-		shortdesc: 'temperature too cold',
-		details: 'Three of the radiators don\'t seem to be working.',
-		anonymous: 1,
-		uid: 'abcd1234',
-		user: '00002368'
-	},
-	{
-		date: '01/01/2014',
-		time: '14:23',
-		dept: 'Lung Function',
-		unit: 'North Entrance',
-		room: 'N15',
-		urgency: 'low',
-		category: 'facilities',
-		subcategory: 'cleaning',
-		shortdesc: 'dirty toilet',
-		details: 'Men\'s toilet 2 is dirty.',
-		anonymous: 1,
-		uid: 'defg4567',
-		user: '00003351'
-	},
-	{
-		date: '01/01/2014',
-		time: '19:53',
-		dept: 'Urology Centre',
-		room: 'N1a',
-		urgency: 'high',
-		category: 'utility',
-		subcategory: 'equipment',
-		shortdesc: 'missing equipment',
-		details: 'Cabinet 3 needs restocking.',
-		anonymous: 1,
-		uid: 'hijk3526',
-		user: '00008363'
-	}
-];
 Template.issues.helpers({
-	issues: issuesFixture
+	issues: function() {
+		return Issues.find();
+	}
 });
---
+++ collections/issues.js
@@ -0,0 +1,6 @@
+/**
+ * Grumble Button issues collection routines for client and server. Issues
+ *	result from grumbling.
+ */
+
+Issues = new Meteor.Collection('issues');
