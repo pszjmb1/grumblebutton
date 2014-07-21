3f63c90c36fb99b970d718d58085785a7e52f3f9
eyha <psyea2@nottingham.ac.uk>
2014-07-21 12:26:30 +0100

    Changed the main file so that when the user logs out, it redirects back to the main page. This ensures that it always goes to the correct page on login amongst other things.

--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@
 issuesHandle = Meteor.subscribeWithPagination('newIssues',10);
 // Single issue and Comments subscription reacts when the current issue
 // changes.
+var loggedIn = false;
 Deps.autorun(function() {
 	Meteor.subscribe('singleIssue', Session.get('currentIssueId'));
 	//Meteor.subscribe('issues', Session.get('currentIssueId'));
@@ -12,6 +13,14 @@ Deps.autorun(function() {
 	//Meteor.subscribe('users');	
 	Meteor.subscribe('users');
 
+	if(!Meteor.userId()){
+		if(loggedIn){
+			window.location.replace("/");
+		}
+		else{
+			loggedIn = true;
+		} 
+	}
 });
 
 Meteor.subscribe('notifications');
