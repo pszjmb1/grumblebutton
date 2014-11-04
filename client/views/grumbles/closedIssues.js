f9221a923bf03e9fc9e5280eacbc37c00d5d13b1
Jesse Blum <pszjmb@exmail.nottingham.ac.uk>
2014-11-04 12:37:53 +0000

    Fixed the broken search box. It previously connected searches across users, as well as called out to the server (inefficient), did unnecessary page refreshes, and didn't indicate to the user what the search criteria was when leaving and then returning to the page.

--- client/views/grumbles/closedIssues.js
+++ client/views/grumbles/closedIssues.js
@@ -2,98 +2,43 @@
  * Template helper for closedIssues
  */
 
-// issueClosed field denotes the closed status of issues
-// Set value of closedIssueSearch field denotes that user wants to search this issue
-var global='';
+Session.setDefault("closed_issues_keyphrase","");
+
 Template.closedIssues.helpers({
 	closedIssues: function() {
 		// Returning closed issues 
-		return Issues.find({issueClosed:1}, {sort: {submitted: -1}, 
+		return Issues.find({issueClosed:1, details: new RegExp(Session.get("closed_issues_keyphrase"))}, {sort: {submitted: -1}, 
 			limit: closedIssuesHandle.limit()});
 	},
-	findSeachedIssues: function() {
-		return Issues.find({issueClosed:1, closedIssueSearch:1}).count();		
-		
-	},
-	searchedIssues: function() {
-		return Issues.find({issueClosed:1, closedIssueSearch:1}, {sort: {submitted: -1}});
-	},
+	
 	// Determine if curent list of issues is ready
 	issuesReady: function() {
 		return !closedIssuesHandle.loading();
 	},
+
 	// Determine if all issues have been loaded
 	allIssuesLoaded: function() {
 		return !closedIssuesHandle.loading() &&
 			Issues.find({issueClosed: 1}).count() < closedIssuesHandle.loaded();
+	},
+	keyphrase: function () {
+		return Session.get("closed_issues_keyphrase");
 	}
 });
 
-// Unsetting the closedIssueSearch field to be use in next search
 Template.closedIssues.events({
 	// Handle the load more event
-	'click .load-more': function(e) 
-	{
+	'click .load-more': function(e) {
 		e.preventDefault();
 		closedIssuesHandle.loadNextPage();
-	},
-	'click #textbox': function(e) 
-	{
-		jQuery('#textbox').on('input', function() 
-		{
-			issues = Issues.find();
-			if(issues)
-			{
-				issues.forEach(function (myDoc)
-				{
-					Meteor.call('toggleClosedSearch', myDoc._id, 0, function(error) {
-						if(error){
-							throwError(error.reason || "Unknown error resetting search field");
-						}
-					});
-				});
-			}		
-		});
 	}
 });
 
-// Updating the closedIssueSearch field to retrieve onlye the required one closed issues
+// Setting the closed_issues_keyphrases from the close_search_textbox on submit
 Template.closedIssues.events({
 	'click #search': function(e) {
 		e.preventDefault();
-		global = document.querySelector('[name=keyword]').value;
-		//Session.set("key",document.querySelector('[name=keyword]').value );
-		Session.set("key", global);
-		var issues = Issues.find({issueClosed:1});
-		var regEx='', location='', details='';
-
-		// Preventing the user to search without entering the search keyword
-		if(!Session.get("key"))
-		{
-			alert('Enter element to search');
-		}
-		if(Session.get("key"))
-		{
-			regEx = new RegExp("^.*"+Session.get("key")+".*","gi");
-			if(issues)
-			{
-				issues.forEach( function(myDoc)
-				{
-					location = myDoc.location || "";
-					details = myDoc.details || myDoc.shortdesc;
-					if(details.match(regEx) || location.match(regEx))
-					{
-						Meteor.call('toggleClosedSearch', myDoc._id, 1, function(error) {
-							if(error){
-								throwError(error.reason || "Unknown error setting search field");
-							}
-						});
-					}
-				});
-			}
-
-		}
-		return false;
+		var keyphrase = $('#close_search_textbox').val();
+		Session.set("closed_issues_keyphrase", keyphrase);
 	}
 });
-
--- client/views/grumbles/issues.js
+++ client/views/grumbles/issues.js
@@ -2,32 +2,26 @@
  * Template helper for issues
  */
 
-// issueClosed field denotes the closed status of issues
-// Set value of IssueSearch field denotes that user wants to search this issue
-var global='';
+Session.setDefault("issues_keyphrase","");
 Template.issues.helpers({
 	issues: function() {
-		return  Issues.find({issueClosed:0}, {sort: {date: -1, time:-1}, 
+		return  Issues.find({issueClosed:0, details: new RegExp(Session.get("issues_keyphrase"))}, {sort: {date: -1, time:-1}, 
 			limit: openIssuesHandle.limit()});
 	},
 	// Determine if curent list of issues is ready
 	issuesReady: function() {
 		return !openIssuesHandle.loading();
 	},
-	// Retrieve the count of issues that has been searched
-	findSeachedIssues: function() {
-		return Issues.find({issueClosed:0, issueSearch:1}).count();		
-	},
-	// Retrieve the issues that has been searched
-	searchedIssues: function() {
-		return Issues.find({issueClosed:0, issueSearch:1}, {sort: {date: -1, time:-1}});
-	},
 	// Determine if all issues have been loaded
 	allIssuesLoaded: function() {
 		return !openIssuesHandle.loading() &&
 			Issues.find({issueClosed: 0}).count() < openIssuesHandle.loaded();
+	},
+	keyphrase: function () {
+		return Session.get("issues_keyphrase");
 	}
 }); 
+
 
 // Handle the load more event
 Template.issues.events({
@@ -37,62 +31,12 @@ Template.issues.events({
 	}
 });
 
-// Unsetting the issueSearch field to be use in next search
-Template.issues.events(
-{
-	'click #textbox': function(e) 
-	{
-		jQuery('#textbox').on('input', function() 
-		{
-			issues = Issues.find();
-			if(issues)
-			{
-				issues.forEach(function (myDoc)
-				{
-					Meteor.call('toggleOpenSearch', myDoc._id, 0, function(error) {
-						if(error){
-							throwError(error.reason || "Unknown error toggleSearch");
-						}
-					});
-				});
-			}	
-		});
-	}
-});
-
-// Setting the issueSearch field to be use to return only required issues
+// Sets the issues_keyphrase session varialble to the contents of the search_textbox
 Template.issues.events({
 	'click #search': function(e) {
-		global = document.querySelector('[name=keyword]').value;
-		Session.set("key", global);
-		var issues = Issues.find();
-		var regEx='', location='', details='';
-
-		//Preventing the user to search without entering the keyword
-		if(!Session.get("key"))
-		{
-			alert('Enter element to search');
-		}
-		if(Session.get("key"))
-		{
-			regEx = new RegExp("^.*"+Session.get("key")+".*","gi");
-			issues.forEach( function(myDoc)
-			{
-				location = myDoc.location;
-				details = myDoc.details;
-				
-				// Checking in every part of the form where the search keyword can be found
-				if(location.match(regEx) || details.match(regEx))
-				{
-					Meteor.call('toggleOpenSearch', myDoc._id, 1, function(error) {
-						if(error){
-							throwError(error.reason || "Unknown error toggleSearch");
-						}
-					});
-				}
-			});
-		}
-		return false;
+		e.preventDefault();
+		var keyphrase = $('#search_textbox').val();
+		Session.set("issues_keyphrase", keyphrase);
 	}
 });
 
